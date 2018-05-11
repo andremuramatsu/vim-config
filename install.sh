0d28f710b86b87beb2be41155eef34fd0f09dc84
Andre <andre@mt4.com.br>
2018-05-11 00:11:58 -0300

    Usability and validations

--- install.sh
+++ install.sh
@@ -2,61 +2,109 @@
 
 DEPENDENCIES="git ctags pear"
 
-for DEP in $DEPENDENCIES; do
-    echo "Testing ${DEP}"
-    if [ -z "$(which ${DEP})" ] || [ ! -x "$(which ${DEP})" ]; then
-        echo "You must install ${DEP} before"
-        exit 1
-    fi
-done
+function echo_warn {
+    echo -e "\033[033m${1}\033[m"
+}
 
-# PHPMD and PHPCS
-pear channel-discover pear.pdepend.org
-pear channel-discover pear.phpmd.org
+function echo_success {
+    echo -e "\033[032m${1}\033[m"
+}
 
-if [ $(pear list|grep PHP_CodeSniffer|wc -l) != "1" ]; then
-    pear install PHP_CodeSniffer
-fi
+function echo_info {
+    echo -e "\033[038m${1}\033[m"
+}
 
-if [ $(pear list -c pear.phpmd.org|grep PHP_PMD|wc -l) != "1" ]; then
-    pear install phpmd/PHP_PMD 
-fi
+function echo_error {
+    echo -e "\033[031m${1}\033[m"
+}
 
-echo "All dependenciess resolved"
-
-# Removing old vim config
-rm -Rf ~/.vim
-mkdir -p ~/.vim/autoload
-mkdir -p ~/.vim/bundle
-
-# Installing Pathogen
-rm -Rf /tmp/vim-pathogen 2> /dev/null
-git clone https://github.com/tpope/vim-pathogen/ /tmp/vim-pathogen
-cp /tmp/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload
-rm -Rf /tmp/vim-pathogen 2> /dev/null
-
-# Installing bundle packages
 function gitclone {
     URL=$1
     PKG=$2
+    TGT=$3
 
-    echo "Testing vim package ${PKG}"
+    if [ -z "${TGT}" ]; then
+        TGT="~/.vim/bundle/${PKG}"
+    fi
 
-    if [ ! -d ~/.vim/bundle/$PKG ]; then
-        git clone $URL ~/.vim/bundle/$PKG 2>/dev/null 1>/dev/null
+    echo_info "Testing vim package ${PKG} ${TGT}"
+
+    if [ ! -d "${TGT}" ]; then
+        git clone $URL $TGT 2>&1 > /dev/null
         if [ $? -gt 0 ]; then
-            echo "Failure installing ${PKG}"
+            echo_error "Failure installing ${PKG}"
             return 1
         else
-            echo "Package ${PKG} installed";
+            echo_success "Package ${PKG} installed";
             return 0
         fi
     else
-        echo "Package ${PKG} already exists"
+        echo_warn "Package ${PKG} already exists"
         return 1
     fi
 
 }
+
+# Testing SO dependencies
+for DEP in $DEPENDENCIES; do
+    echo_info "Testing ${DEP}"
+    if [ -z "$(which ${DEP})" ] || [ ! -x "$(which ${DEP})" ]; then
+        echo_error "You must install ${DEP} before"
+        exit 1
+    fi
+done
+echo_success "SO dependencies ok"
+
+# PHPMD and PHPCS
+if [ $(pear list -c pear.php.net |grep PHP_CodeSniffer|wc -l) != "1" ]; then
+    CMD="sudo pear install PHP_CodeSniffer"
+    
+    ${CMD}
+    if [ $? -gt 0 ]; then
+        echo_error "You must install PHP_CodeSniffer as root before continue"
+        echo_warn "$CMD"
+        exit 1
+    fi
+fi
+
+if [ $(pear list -c pear.phpmd.org|grep PHP_PMD|wc -l) != "1" ]; then
+    CMD="sudo pear channel-discover pear.pdepend.org"
+    CMD="${CMD} && sudo pear channel-discover pear.phpmd.org"
+    CMD="${CMD} && sudo pear channel-update pear.pdepend.org"
+    CMD="${CMD} && sudo pear channel-update pear.phpmd.org"
+    CMD="${CMD} && sudo pear install phpmd/PHP_PMD" 
+
+    ${CMD}
+    if [ ! $? -eq 0 ]; then
+        echo_error"You must install PHP_CodeSniffer as root before continue"
+        echo_warn "$CMD"
+        exit 1
+    fi
+
+fi
+
+echo_success "All dependenciess resolved"
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+
+# Installing Pathogen
+echo_info "Testing vim package pathogen"
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then 
+    rm -Rf /tmp/vim-pathogen 2> /dev/null
+    git clone https://github.com/tpope/vim-pathogen/ /tmp/vim-pathogen 2>&1 > /dev/null
+    if [ $? -gt 0 ]; then
+        echo_error "Failure installing pathogen"
+        exit 1
+    fi
+
+    cp /tmp/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload
+    rm -Rf /tmp/vim-pathogen 2> /dev/null
+    echo_success "Package pathogen installed"
+else
+    echo_warn "Package already exists"
+fi
+
+# Installing bundle packages
 gitclone 'https://github.com/majutsushi/tagbar.git' tagbar
 gitclone 'https://github.com/godlygeek/tabular.git' tabular
 gitclone 'https://github.com/tomtom/tlib_vim.git' tlib_vim
